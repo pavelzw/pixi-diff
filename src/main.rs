67f4bc4f4499e19114df62abb9ffb22d3a351aa6
Pavel Zwerschke <pavelzw@gmail.com>
2025-01-25 01:57:06 +0100

    Implement first version

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use std::path::PathBuf;
 use clap::Parser;
 use clap_verbosity_flag::Verbosity;
 
-use anyhow::Result;
+use miette::IntoDiagnostic;
 use tracing_log::AsTrace;
+
+use rattler_lock::LockFile;
+
+use pixi::{
+    diff::{LockFileDiff, LockFileJsonDiff},
+    Project,
+};
 
 /* -------------------------------------------- CLI -------------------------------------------- */
 
@@ -18,6 +25,10 @@ struct Cli {
     /// Second lockfile to be compared.
     plus_file: PathBuf,
 
+    /// Pixi manifest file
+    #[arg(long)]
+    manifest_path: Option<PathBuf>,
+
     #[command(flatten)]
     verbose: Verbosity,
 }
@@ -25,8 +36,7 @@ struct Cli {
 /* -------------------------------------------- MAIN ------------------------------------------- */
 
 /// The main entrypoint for the pixi-diff CLI.
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> miette::Result<()> {
     let cli = Cli::parse();
 
     tracing_subscriber::FmtSubscriber::builder()
@@ -36,7 +46,16 @@ async fn main() -> Result<()> {
     tracing::debug!("Starting pixi-diff CLI");
     tracing::debug!("Parsed CLI options: {:?}", cli);
 
-    
+    let minus_lockfile = LockFile::from_path(&cli.minus_file).into_diagnostic()?;
+    let plus_lockfile = LockFile::from_path(&cli.plus_file).into_diagnostic()?;
+
+    let project = Project::load_or_else_discover(cli.manifest_path.as_deref())?;
+
+    let diff = LockFileDiff::from_lock_files(&minus_lockfile, &plus_lockfile);
+    let json_diff = LockFileJsonDiff::new(&project, diff);
+    let json = serde_json::to_string_pretty(&json_diff).expect("failed to convert to json");
+
+    println!("{}", json);
 
     Ok(())
 }
