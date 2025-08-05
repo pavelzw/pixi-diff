626fa84b8427f4170194be42700a20360781e7a6
dennis-wey <dennis.weyland@web.de>
2025-08-05 09:17:30 +0200

    use WorkSpaceDiscoverer to discover path

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::{
+    env::current_dir,
     io::Read,
     path::{Path, PathBuf},
     str::FromStr,
@@ -9,7 +10,7 @@ use pixi::{
     diff::{LockFileDiff, LockFileJsonDiff},
     workspace::Workspace,
 };
-use pixi_manifest;
+use pixi_manifest::{DiscoveryStart, WorkspaceDiscoverer};
 use rattler_lock::LockFile;
 
 #[derive(Debug, Clone)]
@@ -38,14 +39,20 @@ pub fn diff(before: Input, after: Input, manifest_path: Option<&Path>) -> miette
     let before_lockfile = LockFile::from_str(&before_content).into_diagnostic()?;
     let after_lockfile = LockFile::from_str(&after_content).into_diagnostic()?;
 
-    let workspace = match manifest_path {
-        Some(path) if path.exists() => match Workspace::from_path(path) {
-            Ok(project) => Some(project),
-            Err(pixi_manifest::LoadManifestsError::Io(_)) => None,
-            Err(e) => return Err(e.into()),
-        },
+    let discover_start = match manifest_path {
+        Some(path) if path.is_file() => DiscoveryStart::ExplicitManifest(path.to_path_buf()),
+        Some(path) if path.is_dir() => DiscoveryStart::SearchRoot(path.to_path_buf()),
+        _ => DiscoveryStart::SearchRoot(current_dir().into_diagnostic()?),
+    };
+
+    let workspace = match WorkspaceDiscoverer::new(discover_start).discover() {
+        Ok(Some(manifests)) => {
+            let manifest_path = &manifests.value.workspace.provenance.path;
+            Some(Workspace::from_path(manifest_path)?)
+        }
         _ => None,
     };
+
     let diff = LockFileDiff::from_lock_files(&before_lockfile, &after_lockfile);
     let json_diff = LockFileJsonDiff::new(workspace.as_ref(), diff);
     Ok(serde_json::to_string_pretty(&json_diff).expect("failed to convert to json"))
